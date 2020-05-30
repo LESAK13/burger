1593d97b9cbcc7c552b451bb7b8d9630b9c6896a
LESAK13 <lesak13@gmail.com>
2020-05-30 16:55:13 -0400

    basic css added

--- db/seeds.sql
+++
@@ -1,9 +0,0 @@
-USE burgers_db;
-
-INSERT INTO burgers (burger_name) VALUES ('Bacon Cheeseburger');
-INSERT INTO burgers (burger_name) VALUES ('Mushroom Swiss Burger');
-INSERT INTO burgers (burger_name, devoured) VALUES ('Pizza Burger', true);
-
-
-USE burgers_db;
-SELECT * FROM burgers;

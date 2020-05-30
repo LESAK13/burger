cb4ea0d685ae2456e70b6cb4bde9fdea1b708c2c
LESAK13 <lesak13@gmail.com>
2020-05-29 21:06:24 -0400

    initial db folder, schemas, seeds set up

---
+++ db/seeds.sql
@@ -0,0 +1,9 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name) VALUES ('Bacon Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('Mushroom Swiss Burger');
+INSERT INTO burgers (burger_name) VALUES ('Pizza Burger');
+
+
+USE burgers_db;
+SELECT * FROM burgers;

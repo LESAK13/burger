c4b88ebb06f23aa1267d780877d6ba6c7a14f9c8
LESAK13 <lesak13@gmail.com>
2020-06-02 18:19:35 -0400

    deployment test

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,18 @@
 var mysql = require("mysql");
+var connection;
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "rootpass",
+    database: "burgers_db"
+  });
+}
 
-var connection = mysql.createConnection({
-  host: "us-cdbr-east-05.cleardb.net",
-  port: 3306,
-  user: "b35bcbcb605beb",
-  password: "ed2bd77b",
-  database: "burgers_heroku_4fed1338e7b47c8db"
-});
-
-connection.connect(function(err) {
+connection.connect(function (err) {
   if (err) {
     console.error("error connecting: " + err.stack);
     return;
